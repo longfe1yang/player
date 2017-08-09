9b977b873837f5d18e8fa265d951489e0abe13f8
fuji <yanglongfei71937315@gmail.com>
2017-08-09 21:13:53 +0800

    currentTime and allTime were added

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-'use strict'
+'use strict';
 
 var player = e('#id-audio-player');
 var play = e('#id-audio-play');
 var pause = e('#id-audio-pause');
-var cuttentTime = e('#id-audio-cuttentTime');
-var allTime = e('#id-audio-allTime')
+var currentTime = e('#id-audio-currentTime');
+var allTime = e('#id-audio-allTime');
+
 bindEvent(play, 'click', () => {
-    player.play();
-})
+  player.play();
+});
+
 bindEvent(pause, 'click', () => {
-    player.pause();
+  player.pause();
+});
+
+bindEvent(player, 'canplay', () => {
+  allTime.innerHTML = '总时长:' + player.duration.toFixed(2);
+
+  var cT = setInterval(() => {
+    currentTime.innerHTML = '当前时间:' + player.currentTime.toFixed(2);
+  }, 1000)
 })
